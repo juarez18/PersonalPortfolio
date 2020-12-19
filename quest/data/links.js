09908c3de4122f5e1b8dbcad13e405e62085dfa0
juarez18 <marcusjuarez18@gmail.com>
2020-12-18 18:57:28 -0700

    update

---
+++ quest/data/links.js
@@ -0,0 +1,27 @@
+const requestURL = "data/links.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //console.table(jsonObject); 
+    const links = jsonObject['links'];
+    for (let i = 0; i < links.length; i++ ) {
+        let li = document.createElement("li");
+        let img = document.createElement("img");
+        let a = document.createElement("a");
+
+        img.setAttribute("alt", links[i].label)
+        img.setAttribute("src", links[i].image)
+        a.setAttribute("href", links[i].url);
+
+        a.appendChild(img);
+        li.appendChild(a);
+
+        document.getElementById('navigation').appendChild(li);
+
+    }
+    
+});
+
